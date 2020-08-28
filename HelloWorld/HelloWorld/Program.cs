28d8de1f27457a8e14f5b11e8f3906712a7406c6
Brian Reeves <1949commander@gmail.com>
2020-08-27 19:53:34 -0700

    First simple C sharp programs
    Upload first C sharp assignments

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello \" Studebaker\" is here today!");
+        Console.ReadLine();
+        }
+    }
