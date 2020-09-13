8cc9fb40d17e05a2f7e8b3bc1d504d4623e44370
Brian Reeves <1949commander@gmail.com>
2020-09-12 23:23:27 -0700

    Insurance App
    Upload Insurance app

---
+++ Insuranace_app/Insuranace_app/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insuranace_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age ?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool yourDui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int yourTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Age = " + yourAge);
+            Console.WriteLine("DUI's" + yourDui);
+            Console.WriteLine("Tickets?" + yourTickets);
+            bool ageTrue = yourAge > 15;
+            bool tickTrue = yourTickets > 3;
+            bool qualTrue = ageTrue && tickTrue && yourDui;
+            Console.WriteLine("Qualified " + qualTrue);
+
+            Console.ReadLine();
+
+            //Console.WriteLine(true && false);     FALSE
+            //Console.WriteLine(true && true);      TRUE
+            //Console.WriteLine(false && false);    FALSE
+
+            //Console.WriteLine(true || false);     TRUE
+            //Console.WriteLine(true || true);      TRUE
+            //Console.WriteLine(false || false);    FALSE
+
+            //Console.WriteLine(true == false);     FALSE
+            //Console.WriteLine(true == true);      TRUE
+            //Console.WriteLine(false == false);    TRUE
+
+            //Console.WriteLine(true ^ false);      TRUE
+            //Console.WriteLine(true ^ true);       FALSE
+            //Console.WriteLine(false ^ false);     FALSE
+            //Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Test-App-1/Test-App-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_App_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int packageWidth = 10;
+            //int packageHeight = 25;
+            //bool tooBig = (packageHeight > 25 || packageWidth > 25);
+            //Console.WriteLine(tooBig);
+            //Console.ReadLine();
+            //string firstName = "Jesse";
+            //string lastName = "Johnson";
+            //string socialSecurityNumber = "111-11-1111";
+
+            //bool isAuthorized = (firstName == "Jesse" && lastName == "Johnson" && socialSecurityNumber == "111-11-1111");
+            //Console.WriteLine(isAuthorized);
+
+            //int bankAccountBalance = 500;
+            //bool isInARockBand = false;
+            //string name = "Jesse";
+
+            //bool isReallyCool = (bankAccountBalance > 100000 || isInARockBand || name == "Jesse");
+
+            //Console.WriteLine(isReallyCool);
+
+            //bool result = (true && (true || false));
+            //Console.WriteLine(result);
+
+            int num1 = 7;
+            int num2 = 12;
+            int num3 = 23;
+            int num4 = 14;
+            int num5 = 8;
+            int num6 = 5;
+
+            bool result = (num1 > num2 && (num3 == num4 || num5 > num6));
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
