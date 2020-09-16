442fb91e48a55542499cb1d0d820ced92e278e12
Brian Reeves <1949commander@gmail.com>
2020-09-15 21:04:42 -0700

    Upload the branching assignment
    Completed Package Calulation assignment

---
+++ BRANCHING_ASSIGNMENT/BRANCHING_ASSIGNMENT/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRANCHING_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter package weight: ");
+            double pakgWeight = Convert.ToDouble(Console.ReadLine());
+            if (pakgWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Package Height: ");
+                double pakgHeight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please Enter Package Width: ");
+                double pakgWidth = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please Enter Package Length: ");
+                double pakgLength = Convert.ToDouble(Console.ReadLine());
+                double pakgSize = pakgHeight + pakgWidth + pakgLength;
+                if (pakgSize > 50)
+                {
+                    Console.WriteLine("Package too Large to be shipped via Package Express. Have a good day.");
+                }
+                else
+                {
+                    double pakgCost = (pakgHeight * pakgWidth * pakgLength * pakgWeight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + pakgCost);
+                    Console.WriteLine("Thank you.");
+                }
+                    
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Test_app_2/Test_app_2/Program.cs
+++ Test_app_2/Test_app_2/Program.cs
@@ -10,89 +10,94 @@ namespace Test_app_2
     {
         static void Main(string[] args)
         {
-            int age = 21;
-            if (age <= 20)
-            {
-                Console.WriteLine("No you may not purchase that bottle of wine");
-            }
-            else
-            {
-                Console.WriteLine("There you go!");
-            }
+            /*            int age = 21;
+                        if (age <= 20)
+                        {
+                            Console.WriteLine("No you may not purchase that bottle of wine");
+                        }
+                        else
+                        {
+                            Console.WriteLine("There you go!");
+                        }
 
-            int age1 = 21;
-            if (age1 >= 25)
-            {
-                Console.WriteLine("You may rent a car");
-            }
-            else
-            {
-                Console.WriteLine("You cannot rent a car");
-            }
+                        int age1 = 21;
+                        if (age1 >= 25)
+                        {
+                            Console.WriteLine("You may rent a car");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You cannot rent a car");
+                        }
 
-            int num1 = 7;
-            int num2 = 18;
-            int num3 = 12;
-            if (num2 > num1 && num3 < num2)
-            {
-                Console.WriteLine("The second number is larger than the others.");
-            }
+                        int num1 = 7;
+                        int num2 = 18;
+                        int num3 = 12;
+                        if (num2 > num1 && num3 < num2)
+                        {
+                            Console.WriteLine("The second number is larger than the others.");
+                        }
 
-            if (true && true && true && true && num1 == 20)
-            {
-                Console.WriteLine("Everything is true!");
-            }
-            else
-            {
-                Console.WriteLine("There was a false somewhere!");
-            }
+                        if (true && true && true && true && num1 == 20)
+                        {
+                            Console.WriteLine("Everything is true!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("There was a false somewhere!");
+                        }
 
-            int packageWeight = 30;
-            int packageLength = 20;
-            if (packageWeight > 40 && packageLength > 20)
-            {
-                Console.WriteLine("This package is too big to ship.");
-            }
-            else
-            {
-                Console.WriteLine("Good to go!");
-            }
+                        int packageWeight = 30;
+                        int packageLength = 20;
+                        if (packageWeight > 40 && packageLength > 20)
+                        {
+                            Console.WriteLine("This package is too big to ship.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Good to go!");
+                        }
 
-            if (num1 < 7 || num1 ==12)
-            {
-                Console.WriteLine("At least one value is true.");
-            }
+                        if (num1 < 7 || num1 ==12)
+                        {
+                            Console.WriteLine("At least one value is true.");
+                        }
 
-            if (num1 == 12 || num1 ==18)
-            {
-                Console.WriteLine("At least one value is true.");
-            }
-            else
-            {
-                Console.WriteLine("No value is true.");
-            }
+                        if (num1 == 12 || num1 ==18)
+                        {
+                            Console.WriteLine("At least one value is true.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No value is true.");
+                        }
 
-            if (num1 == 12 || num1 == 18 || num1 == 20 || num1 == 7)
-            {
-                Console.WriteLine("Something is true.");
-            }
+                        if (num1 == 12 || num1 == 18 || num1 == 20 || num1 == 7)
+                        {
+                            Console.WriteLine("Something is true.");
+                        }
 
-            string role = "admin";
-            if (role == "admin" || role == "administrator")
-            {
-                Console.WriteLine("You may have access to the entire software system.");
-            }
+                        string role = "admin";
+                        if (role == "admin" || role == "administrator")
+                        {
+                            Console.WriteLine("You may have access to the entire software system.");
+                        }
 
-            if (num1 < 18 && ((num1 == 7 )|| num1 == 2) && ((num1 == 8 && num1 > 20) || (num1 < 20 && num1 > 2)))
-            {
-                Console.WriteLine("Wow, that was confusing.");
-            }
+                        if (num1 < 18 && ((num1 == 7 )|| num1 == 2) && ((num1 == 8 && num1 > 20) || (num1 < 20 && num1 > 2)))
+                        {
+                            Console.WriteLine("Wow, that was confusing.");
+                        }
 
-            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
-            {
-                Console.WriteLine(result);
-            }
-            
+                        string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+                        {
+                            Console.WriteLine(result);
+                        }*/
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number";
+
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
