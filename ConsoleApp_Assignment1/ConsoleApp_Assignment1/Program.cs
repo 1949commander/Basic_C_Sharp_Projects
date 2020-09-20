7e69e2cbab23a1d634cf1a8911f938c25e53cac8
Brian Reeves <1949commander@gmail.com>
2020-09-20 13:27:20 -0700

    Upload assignment 197
    Upload first part of the app

---
+++ ConsoleApp_Assignment1/ConsoleApp_Assignment1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vehicles = { "Car", "Truck", "Van", "SUV" };
+
+            Console.WriteLine("Please input a color:");
+            string newString = Console.ReadLine();
+            foreach (string i in vehicles)
+            {
+                Console.WriteLine(i + newString);
+            }
+                 Console.ReadLine();
+        }
+    }
+}
