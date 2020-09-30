e7c904b8b72529787e33db930e4438e7a81ed9b7
Brian Reeves <1949commander@gmail.com>
2020-09-30 10:40:39 -0700

    Update Program.cs
    Uploaded final version

--- ConsoleApp_Assignment1/ConsoleApp_Assignment1/Program.cs
+++ ConsoleApp_Assignment1/ConsoleApp_Assignment1/Program.cs
@@ -11,72 +11,87 @@ namespace ConsoleApp_Assignment1
     {
         static void Main(string[] args)
         {
-            //string[] vehicles = { "Car ", "Truck ", "Van ", "SUV " };
+            string[] vehicles = { "Car ", "Truck ", "Van ", "SUV " };
 
-            //Console.WriteLine("Please input a color:");
-            //string newString = Console.ReadLine();
-            //for (int i = 0; i < vehicles.Length; i++)
-            //{
-            //    vehicles[i] += newString;
-            //}
+            Console.WriteLine("Please input a color:");
+            string newString = Console.ReadLine();
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                vehicles[i] += newString;
+            }
 
-            //foreach (string i in vehicles)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //int a = 0;
-            //while (a < 5)
-            //{
-            //    Console.WriteLine(a);
-            //    a++;
-            //}
-            //int b = 0;
-            //while (b <= 5)
-            //{
-            //    Console.WriteLine(b);
-            //    b++;
-            //}
+            foreach (string i in vehicles)
+            {
+                Console.WriteLine(i);
+            }
+            int a = 0;
+            while (a < 5)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            int b = 0;
+            while (b <= 5)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
 
-            //List<string> carNames = new List<string>();
-            //carNames.Add("Dodge Dart");
-            //carNames.Add("Nissan Sentra");
-            //carNames.Add("Ford Focus");
-            //carNames.Add("Chevy Bolt");
-            //carNames.Add("Toyota Corola");
-            //carNames.Add("Honda Civic");
-            //Console.WriteLine("Enter Name of Car:");
-            //string txtSearch = Console.ReadLine();
+            List<string> carNames = new List<string>();
+            carNames.Add("Dodge Dart");
+            carNames.Add("Nissan Sentra");
+            carNames.Add("Ford Focus");
+            carNames.Add("Chevy Bolt");
+            carNames.Add("Toyota Corola");
+            carNames.Add("Honda Civic");
+            Console.WriteLine("Enter Name of Car:");
+            string txtSearch = Console.ReadLine();
 
-            //foreach (var name in carNames)
-            //    if (name == txtSearch)
-            //    {
-            //        int i = carNames.IndexOf(name);
-            //        Console.WriteLine(i);
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("No match try again");
-            //    }
+            foreach (var name in carNames)
+                if (name == txtSearch)
+                {
+                    int i = carNames.IndexOf(name);
+                    Console.WriteLine(i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No match try again");
+                }
 
             List<string> peoplesNames = new List<string>();
-            List<string> noDup = new List<string>();
+            
             peoplesNames.Add("Joe Smith");
             peoplesNames.Add("John Jones");
             peoplesNames.Add("Jane Hellen");
             peoplesNames.Add("Nancy Smith");
             peoplesNames.Add("Mary Joplin");
-            peoplesNames.Add("Joe Smith");
+            peoplesNames.Add("Joe Smith"); // dup
             peoplesNames.Add("Zack Wright");
+            peoplesNames.Add("Mary Joplin"); // dup
 
+            // Make new unique list
+            List<string> noDup = peoplesNames.Distinct().ToList();
 
-            var query = peoplesNames.GroupBy(x => x)
-              .Where(g => g.Count() > 1)
-              .Select(y => new { Element = y.Key, Counter = y.Count() })
-              .ToList();
-            Console.WriteLine(peoplesNames);
+            // Write to console
+            noDup.ForEach(i => Console.WriteLine($"{i}"));
 
+            if(peoplesNames.Count != noDup.Distinct().Count())
+            {
+                
+                Console.WriteLine("Duplicates found");
+            }
+            else
+            {
+                Console.WriteLine("No Duplicate found");
+            }
 
+            IEnumerable<string> duplicates = from x in peoplesNames
+                                          group x by x into g
+                                          where g.Count() > 1
+                                          select g.Key;
+
+            Console.WriteLine("Duplicate elements are: " + String.Join(", ", duplicates));
 
             Console.ReadLine();
         }
