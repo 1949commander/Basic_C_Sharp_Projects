22d0d17e3b74d68f0ec571fd29a6a8782d24a3ab
Brian Reeves <1949commander@gmail.com>
2020-10-06 22:11:59 -0700

    Simple Method
    First upload

---
+++ methods_2_Math/methods_2_Math/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_2_Math
+{
+    public class Class1
+    {
+
+        public static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int MultMethod(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int DivMethod(int x, int y)
+        {
+            return x / y;
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+ }
---
+++ methods_2_Math/methods_2_Math/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_2_Math
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is:" + userNumber);
+
+            int myNum1 = PlusMethod(8, userNumber);
+            int myNum2 = MultMethod(4, userNumber);
+            int myNum3 = DivMethod(15, userNumber);
+            Console.WriteLine("PlusMethod: " + myNum1);
+            Console.WriteLine("MultMethod: " + myNum2);
+            Console.WriteLine("DivMethod: " + myNum3);
+        }
+    }
+}
