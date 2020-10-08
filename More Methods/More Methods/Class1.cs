d68c3cc73a55109d21667ff912b60b0b8c3d9c7b
Brian Reeves <1949commander@gmail.com>
2020-10-08 01:11:12 -0700

    More Methods
    Upload new App

---
+++ More Methods/More Methods/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Methods
+{
+    public class Class1
+    {
+        public static int PlusMethod(int x, int y) => x + y;
+
+        public static double PlusMethod(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static int PlusMethod(string x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ More Methods/More Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Methods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int myNum1 = Class1.PlusMethod(8, 9);
+            Console.WriteLine("Add Intergers: " + myNum1);
+
+            double myNum2 = Class1.PlusMethod(2.3, 3.5);
+            Console.WriteLine("Multiply Decimal: " + myNum2);
+            
+
+            int myNum3 = Class1.PlusMethod(2 , 3);
+            Console.WriteLine("Multiply Decimal: " + myNum3);
+            Console.ReadLine();
+
+        }
+    }
+}
