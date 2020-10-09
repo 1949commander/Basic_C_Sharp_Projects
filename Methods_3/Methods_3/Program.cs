db34d76b18f194d3361ddbff594c874463b7bc95
Brian Reeves <1949commander@gmail.com>
2020-10-08 22:23:49 -0700

    Update Program.cs
    Upload to get question answered

--- Methods_3/Methods_3/Program.cs
+++ Methods_3/Methods_3/Program.cs
@@ -8,14 +8,29 @@ namespace Methods_3
 {
     class Program
     {
+        private static bool isNumber;
+        private static bool result;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter an integer: ");
             int UserNum1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter an optional interger: ");
-            int UserNum2 = Convert.ToInt32(Console.ReadLine());
-            int myNum1 = Class1.MethodOne(UserNum1, UserNum2);
-            Console.WriteLine("Add Integers: " + myNum1);
+            string UserNum2 = Console.ReadLine();
+            bool result = int.TryParse(UserNum2, out var n);
+
+            if (string.IsNullOrEmpty(UserNum2))
+            {
+                int myNum1 = Class1.MethodOne(UserNum1);
+                Console.WriteLine("Add Integers: " + myNum1);
+            }
+            else result == true;
+            {
+                int myNum1 = Class1.MethodOne(UserNum1, n);
+                Console.WriteLine("Add Integers: " + myNum1, + myNum2);
+            }
+
+            
             Console.ReadLine();
         }
     }
