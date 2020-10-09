b81251ac214c6d6acac1849715907ce52ad81ec0
Brian Reeves <1949commander@gmail.com>
2020-10-08 17:53:04 -0700

    Methods 3
    Upload methods 3

---
+++ Methods_3/Methods_3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    public class Class1
+    {
+        public static int MethodOne(int Num1, int Num2 = 12)
+        {
+            return Num1 + Num2;
+        }
+
+    }
+}
---
+++ Methods_3/Methods_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            int UserNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an optional interger: ");
+            int UserNum2 = Convert.ToInt32(Console.ReadLine());
+            int myNum1 = Class1.MethodOne(UserNum1, UserNum2);
+            Console.WriteLine("Add Integers: " + myNum1);
+            Console.ReadLine();
+        }
+    }
+}
